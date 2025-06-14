b1c3f17251eb3db50d15a699a592461c00e28d78
Abdulaziz Ghuloum <aghuloum@gmail.com>
2025-06-14 12:35:52 +0300

    added basic photo url with basic transformations and file extensions

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import pg from "pg";
 import multer from "multer";
+import sharp from "sharp";
 
 import { customAlphabet } from "nanoid";
 
@@ -48,6 +49,93 @@ app.post(
     }
   }
 );
+
+function make_single_trans(trans: string): (img: sharp.Sharp) => void {
+  {
+    const m = trans.match(/^r(\d+)$/);
+    if (m) {
+      return (img) => img.rotate(parseInt(m[1], 10));
+    }
+  }
+  {
+    const m = trans.match(/^c(\d+)x(\d+)$/);
+    if (m) {
+      const w = parseInt(m[1], 10);
+      const h = parseInt(m[2], 10);
+      return (img) => img.resize({ width: w, height: h, fit: "cover" });
+    }
+  }
+  {
+    const m = trans.match(/^e(\d+)x(\d+)-(\d+)x(\d+)$/);
+    if (m) {
+      const x1 = parseInt(m[1]);
+      const y1 = parseInt(m[2]);
+      const x2 = parseInt(m[3]);
+      const y2 = parseInt(m[4]);
+      const width = x2 - x1;
+      const height = y2 - y1;
+      return (img) => img.extract({ width, height, left: x1, top: y1 });
+    }
+  }
+  throw new Error(`invalid transformer ${trans}`);
+}
+
+function make_trans(transs: string[]) {
+  try {
+    const fs = transs.map(make_single_trans);
+    return {
+      error: false as const,
+      f: (img: sharp.Sharp) => {
+        fs.forEach((f) => f(img));
+      },
+    };
+  } catch (err: any) {
+    return { error: true as const, message: err.message };
+  }
+}
+
+const supported_extensions: Record<string, keyof sharp.FormatEnum> = {
+  webp: "webp",
+  jpeg: "jpeg",
+  png: "png",
+};
+
+app.get("/photos/:photo_desc", async (req, res) => {
+  const photo_desc = req.params.photo_desc;
+  if (typeof photo_desc !== "string") {
+    res.status(400).send("invalid request");
+    return;
+  }
+  const parts = photo_desc.split(".");
+  if (parts.length < 2) {
+    res.status(400).send("invalid photo");
+    return;
+  }
+  const [photo_id, ...ops] = parts;
+  const [extension] = ops.splice(-1);
+  const extension_type = supported_extensions[extension];
+  if (!extension_type) {
+    res.status(400).send(`unsupported type ${extension}`);
+  }
+  const transformation = make_trans(ops);
+  if (transformation.error) {
+    res.status(400).send(transformation.message);
+    return;
+  }
+  const out = await pool.query("select data from photo where photo_id = $1", [
+    photo_id,
+  ]);
+  if (out.rows.length === 0) {
+    res.status(404).send("not found");
+  }
+  const image_buffer = out.rows[0].data;
+  const img = sharp(image_buffer);
+  transformation.f(img);
+  img.toFormat(extension_type);
+  res.type(extension_type);
+  img.pipe(res);
+  return;
+});
 
 app.get("/", async (_req, res) => {
   try {
